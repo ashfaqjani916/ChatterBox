c7c9a881d4d4b66f67cbdfa1da2588d78389952a
ashfaqjani916 <ashfaqjani916@gmail.com>
2024-03-15 11:30:27 +0530

    [feat]removing ChakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,99 +1,98 @@
-import  { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
-import {app} from "./firebase"
-import {Box,Button,Container,VStack,Input, HStack} from "@chakra-ui/react"
+import { app } from './firebase'
+import { Box, Button, Container, VStack, Input, HStack } from '@chakra-ui/react'
 import Message from './components/Message'
-import {
-  GoogleAuthProvider, 
-  signInWithPopup,
-  getAuth, 
-  onAuthStateChanged,
-  signOut,
-  User} from "firebase/auth";
+import { GoogleAuthProvider, signInWithPopup, getAuth, onAuthStateChanged, signOut, User } from 'firebase/auth'
 
-import { getFirestore, collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { getFirestore, collection, addDoc, serverTimestamp } from 'firebase/firestore'
 
-
-
-const auth = getAuth(app);
-const db = getFirestore(app);
+const auth = getAuth(app)
+const db = getFirestore(app)
 
 const loginHandler = () => {
   const provider = new GoogleAuthProvider()
 
   signInWithPopup(auth, provider)
-}; 
+}
 
 const logoutHandler = () => {
-  signOut(auth);
-};
-
+  signOut(auth)
+}
 
 function App() {
+  const [user, setUser] = useState(false)
+  const [messages, setMessages] = useState([])
+  const [message, setMessage] = useState('')
 
-  const [user, setUser] = useState(false);
-  const [messages, setMessages] = useState([]);
-  const [message, setMessage] = useState("");
- 
-  const submitHandler = async(e :any, user:User)=>{
-    e.preventDefault();
-  
+  const submitHandler = async (e: any, user: User) => {
+    e.preventDefault()
+
     try {
-      await addDoc(collection(db, "Messages"),{
-        text:{message},
-        uid:  user.uid,
-        photoURL:user.photoURL,
-        createdAt: serverTimestamp()}
-      );
+      await addDoc(collection(db, 'Messages'), {
+        text: { message },
+        uid: user.uid,
+        photoURL: user.photoURL,
+        createdAt: serverTimestamp(),
+      })
 
-      setMessage("");
+      setMessage('')
     } catch (error) {
-      alert(error);
+      alert(error)
     }
-  };
-  
+  }
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (data) => {
-      setUser(data as any);
-    });
+      setUser(data as any)
+    })
 
-    return ()=>{
-      unsubscribe();
-    };
+    return () => {
+      unsubscribe()
+    }
   })
 
   return (
     <>
-    <Box bg={"red.50"}>
-      {
-        user?(<Container h={"100vh"} bg={"ivory"} >
-        <VStack h="full" >
-          <Button onClick={logoutHandler} w={"full"} colorScheme='red'>Logout</Button>
-          
-          <VStack h={'full'} w={'full'} padding={"4"} >
-            {
-              messages.map((item) => (
-                <Message text='item.text' />
-              ))
-            }
-          </VStack>  
-          
-          <form  onSubmit = {(e) => submitHandler(e, user as unknown as User)} style={{ width: "100%"}}>
-            <HStack>
-              <Input value={message} onChange={(e)=> setMessage(e.target.value)} placeholder='Enter a message'/>
-              <Button colorScheme='green'  type="submit" >Send</Button>
+      <Box bg={'red.50'}>
+        {user ? (
+          <HStack>
+            {/* <div className="w-10 h-10 bg-black"></div> */}
+            <Container h={'100vh'} bg={'ivory'}>
+              <HStack>
+                {' '}
+                <VStack bg={'red'} h={'full'}>
+                  hello
+                </VStack>
+                <VStack h="full">
+                  <Button onClick={logoutHandler} w={'full'} colorScheme="red">
+                    Logout
+                  </Button>
+
+                  <VStack h={'full'} w={'full'} padding={'4'}>
+                    {messages.map((item) => (
+                      <Message text="item.text" />
+                    ))}
+                  </VStack>
+
+                  <form onSubmit={(e) => submitHandler(e, user as unknown as User)} style={{ width: '100%' }}>
+                    <HStack>
+                      <Input value={message} onChange={(e) => setMessage(e.target.value)} placeholder="Enter a message" />
+                      <Button colorScheme="green" type="submit">
+                        Send
+                      </Button>
+                    </HStack>
+                  </form>
+                </VStack>
               </HStack>
-             </form>          
-          
-        </VStack>
-      </Container>):(
-      <VStack justifyContent={"center"} h={"100vh"}>
-        <Button onClick={loginHandler}>Sign in with Google</Button>
-      </VStack>)
-      }
-    </Box>
-     
+            </Container>
+          </HStack>
+        ) : (
+          <VStack justifyContent={'center'} h={'100vh'}>
+            <Button onClick={loginHandler}>Sign in with Google</Button>
+          </VStack>
+        )}
+      </Box>
     </>
   )
 }
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,12 +1,12 @@
-import {HStack,Text,Avatar}from "@chakra-ui/react";
+import { HStack, Text, Avatar } from '@chakra-ui/react'
 
-type MessageProps = {text:string, user?:string,uri?:string}
-const Message = ({text,user,uri}: MessageProps) => {
+type MessageProps = { text: string; user?: string; uri?: string }
+const Message = ({ text, user, uri }: MessageProps) => {
   return (
-    <HStack alignSelf={user==='me'?"flex-end":"flex-start"} bg={"gray.100"} paddingY={'2'} paddingX={"4"} borderRadius={"base"}>
-        { user==='other' && <Avatar src={uri}></Avatar>}
-        <Text >{text}</Text>
-        { user==='me' && <Avatar src={uri}></Avatar>}
+    <HStack alignSelf={user === 'me' ? 'flex-end' : 'flex-start'} bg={'gray.100'} paddingY={'2'} paddingX={'4'} borderRadius={'base'}>
+      {user === 'other' && <Avatar src={uri}></Avatar>}
+      <Text>{text}</Text>
+      {user === 'me' && <Avatar src={uri}></Avatar>}
     </HStack>
   )
 }
